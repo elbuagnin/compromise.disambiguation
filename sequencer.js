a89ac80758d363cc0a72e756daf44dcd25ca2e09
Elbuagnin <elbuagnin@gmail.com>
2022-03-10 23:15:24 -0600

    more functionality

--- sequencer.js
+++ sequencer.js
@@ -8,6 +8,9 @@ export default function sequencer(document) {
         if (instructions.parseMethod === 'pattern') {
           const parseData = {'word': instructions.pattern, 'POSes': instructions.POSes}
           parser(document, instructions.parseMethod, parseData);
+        } else if (instructions.parseMethod === 'termList') {
+          const parseData = {'file': instructions.list};
+          parser(document, instructions.parseMethod, parseData);
         }
         break;
       default:
