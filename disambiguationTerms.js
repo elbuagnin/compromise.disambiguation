208949bed95b1052dfa87aca6f457fe6221e5010
elbuagnin <elbuagnin@gmail.com>
2022-03-12 11:47:42 -0600

    use function to filter terms instead of loading all terms

--- disambiguationTerms.js
+++ disambiguationTerms.js
@@ -3,4 +3,7 @@ import * as mfs from './lib/filesystem.js';
 const disambiguationFilePath = './data/disambiguation/';
 const disambiguationTerms = (mfs.loadJSONDir(disambiguationFilePath, true));
 
-export default (disambiguationTerms);
+export default function filterTerms(list) {
+  const filteredTerms = disambiguationTerms.filter(term => (term.list === list));
+  return filteredTerms;
+}
