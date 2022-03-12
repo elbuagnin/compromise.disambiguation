293920929af4116a0112db405e994bf6d3565755
Elbuagnin <elbuagnin@gmail.com>
2022-03-12 12:20:55 -0600

    updating file/directory structure

--- pos-name-table.js
+++ pos-name-table.js
@@ -1,6 +1,6 @@
 import * as mfs from './lib/filesystem.js';
 
-const posNameTableFile = './data/initialize/pos-name-table.json';
+const posNameTableFile = './data/general/pos-name-table.json';
 const posNameTable = mfs.loadJSONFile(posNameTableFile, 'array');
 
 export default function posNameNormalize(posNameFromData) {
--- sequence.js
+++ sequence.js
@@ -1,6 +1,6 @@
 import * as mfs from './lib/filesystem.js';
 
-const file = './sequence.json';
+const file = './data/general/sequence.json';
 const sequence = mfs.loadJSONFile(file, 'array');
 
 export default sequence;
