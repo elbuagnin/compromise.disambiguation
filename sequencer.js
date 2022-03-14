e742487c524b0418782a544e368a5bd07b2cdf23
elbuagnin <elbuagnin@gmail.com>
2022-03-14 14:28:27 -0500

    added parsing by scope of document also moved parse switching to parser.js

--- sequencer.js
+++ sequencer.js
@@ -1,53 +1,40 @@
 import sequence from './sequence.js';
-import * as parse from './parser.js';
+import parse from './parser.js';
 
 export default function sequencer(document) {
-  function parseTree(doc, instruction) {
-     const {parseBy} = instruction;
-     
-     switch (parseBy) {
-      case 'document':
-         parse.byDocument(doc, instruction);
-         break;
-      case 'termList':
-         parse.byTermList(doc, instruction);
-         break;
-      case 'pattern':
-         parse.byPattern(doc, instruction);
-         break;
-      case 'patternFile':
-         parse.byPatternFile(doc, instruction);
-         break;
-      default:
-         break;
-    }
-  }
 
   const sentences = document.sentences();
   sequence.sort((a, b) => a.order - b.order);
 
   sequence.forEach((instruction) =>  {
     console.log(instruction);
-    const {source} = instruction;
+    const {scope} = instruction;
 
-    if (source === 'document') {
-      parseTree(document, instruction);
+    if (scope === 'document') {
+      parse(document, instruction);
    } else {
 
        sentences.forEach((sentence) => {
-         let chunks = sentence;
-         if (sentence.has('#Comma')) {
-           const commas = sentence.match('#Comma');
-           commas.forEach((comma) => {
-             if (comma.ifNo('(#List|#CoordinatingAdjectives)').found) {
-               chunks = chunks.splitAfter(comma);
-             }
-           });
-         }
 
-        chunks.forEach((chunk) => {
-          parseTree(chunk, instruction);
-        });
+         if (scope === 'sentence') {
+           parse(sentence, instruction);
+         } else {
+
+           // Scope = 'phrase' assumed
+           let chunks = sentence;
+           if (sentence.has('#Comma')) {
+             const commas = sentence.match('#Comma');
+             commas.forEach((comma) => {
+               if (comma.ifNo('(#List|#CoordinatingAdjectives)').found) {
+                 chunks = chunks.splitAfter(comma);
+               }
+             });
+           }
+
+          chunks.forEach((chunk) => {
+            parse(chunk, instruction);
+          });
+         }
       });
    }
  });
