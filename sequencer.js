2dc7a824e63ce431dde3334d5e1a2bf9f6da35a3
Elbuagnin <elbuagnin@gmail.com>
2022-03-13 22:56:26 -0500

    added scope to sequencing

--- sequencer.js
+++ sequencer.js
@@ -1,12 +1,7 @@
 import sequence from './sequence.js';
 import * as parse from './parser.js';
 
-
 export default function sequencer(document) {
-
-
-  // Process by Disambiguation Term File first, then ...
-  // Process the document by sentences and then by non-list commas
 
   const sentences = document.sentences();
 
@@ -15,34 +10,37 @@ export default function sequencer(document) {
     console.log(instruction);
     const {parseBy} = instruction;
 
-    sentences.forEach((sentence) => {
-      let chunks = sentence;
-      if (sentence.has('#Comma')) {
-        const commas = sentence.match('#Comma');
-        commas.forEach((comma) => {
-          if (comma.ifNo('(#List|#CoordinatingAdjectives)').found) {
-            chunks = chunks.splitAfter(comma);
+    if (parseBy === 'document') {
+      parse.byDocument(document, instruction);
+   } else {
+
+       sentences.forEach((sentence) => {
+         let chunks = sentence;
+         if (sentence.has('#Comma')) {
+           const commas = sentence.match('#Comma');
+           commas.forEach((comma) => {
+             if (comma.ifNo('(#List|#CoordinatingAdjectives)').found) {
+               chunks = chunks.splitAfter(comma);
+             }
+           });
+         }
+
+        chunks.forEach((chunk) => {
+          switch (parseBy) {
+             case 'termList':
+               parse.byTermList(chunk, instruction);
+               break;
+             case 'pattern':
+               parse.byPattern(chunk, instruction);
+               break;
+             case 'patternFile':
+               parse.byPatternFile(chunk, instruction);
+               break;
+             default:
+               break;
           }
         });
-      }
-
-     chunks.forEach((chunk) => {
-       switch (parseBy) {
-          case 'termList':
-            parse.byTermList(chunk, instruction);
-            break;
-          case 'pattern':
-            parse.byPattern(chunk, instruction);
-            break;
-          case 'document':
-            parse.byDocument(document, instruction);
-            break;
-          default:
-            break;
-       }
-
-
-     });
-   });
+      });
+   }
  });
 }
