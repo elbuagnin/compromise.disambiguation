2790300e9480fca7aa00c2a47eef6ae0fce913d8
Elbuagnin <elbuagnin@gmail.com>
2022-03-21 12:03:40 -0500

    fixed bug caused by lowercasing all letters instead of first letter

--- pos-name-table.js
+++ pos-name-table.js
@@ -4,12 +4,13 @@ const posNameTableFile = './data/general/pos-name-table.json';
 const posNameTable = mfs.loadJSONFile(posNameTableFile, 'array');
 
 export default function posNameNormalize(posNameFromData) {
-   const lowerCasePosNameFromData = posNameFromData.toLowerCase();
+   const dropCapitalPosNameFromData = posNameFromData[0].toLowerCase() + posNameFromData.slice(1);
    let normalizedName = false;
+
    Object.values(posNameTable).forEach(pos => {
-      if (pos.abbreviation === lowerCasePosNameFromData) {
+      if (pos.abbreviation === dropCapitalPosNameFromData) {
         normalizedName = pos.fullname;
-      } else if (pos.fullname === lowerCasePosNameFromData) {
+      } else if (pos.fullname === dropCapitalPosNameFromData) {
         normalizedName = pos.fullname;
       }
    })
