4f12e7afee7cf0294feb9b9b65dc7090a9f7a730
elbuagnin <elbuagnin@gmail.com>
2022-03-23 15:36:14 -0500

    added improbable refactored testing

--- disambiguate.js
+++ disambiguate.js
@@ -1,148 +1,147 @@
-import posNameNormalize from './pos-name-table.js';
-import posTests from './pos-tests.js';
+import posNameNormalize from "./pos-name-table.js";
+import posTests from "./pos-tests.js";
 
 export default function disambiguate(doc, term) {
-   function compromiseTagged(pos) {
-      return '#' + pos.charAt(0).toUpperCase() + pos.slice(1);
-   }
+  function compromiseTagged(pos) {
+    return "#" + pos.charAt(0).toUpperCase() + pos.slice(1);
+  }
 
-   function isPOS(word, pos) {
-
+  function isPOS(word, pos) {
+    function testing(tests) {
       function findChunk(scope) {
-         if (scope === 'phrase') {
+        if (scope === "phrase") {
+          return doc;
+        } else if (scope === "sentence") {
+          if (doc.parent() === doc) {
             return doc;
-         } else if (scope === 'sentence') {
-            if (doc.parent() === doc) {
-               return doc;
-            } else {
-               return doc.parent();
-            }
-         }
+          } else {
+            return doc.parent();
+          }
+        }
       }
 
-      let result = 0;
-      let negativeTests = [];
-      let probableTests = [];
-      let positiveTests = [];
-
-      let testSet = posTests.filter(test => (test.pos === pos));
-
-      if (testSet.length > 0) {
-        negativeTests = testSet.filter(test => (test.type === 'negative'));
-        probableTests = testSet.filter(test => (test.type === 'probable'));
-        positiveTests = testSet.filter(test => (test.type === 'positive'));
+      function score(type) {
+        switch (type) {
+          case "negative":
+            return -2;
+          case "improbable":
+            return -1;
+          case "probable":
+            return 1;
+          case "positive":
+            return 2;
+          default:
+            return 0;
+        }
       }
 
-      negativeTests.forEach((test) => {
-         let pattern = test.pattern.replace('%word%', word);
-         let chunk = findChunk(test.scope);
-         if (chunk.has(pattern)) {
-           console.log('    negative  ' + pos +': ' + pattern);
-           result--;
-           result--;
+      tests.forEach((test) => {
+        let pattern = test.pattern.replace("%word%", word);
+        let chunk = findChunk(test.scope);
+        if (chunk.has(pattern)) {
+          console.log("    " + test.type + "  " + pos + ": " + pattern);
+          result += score(test.type);
         }
       });
+    }
 
-      probableTests.forEach((test) => {
-         let pattern = test.pattern.replace('%word%', word);
-         let chunk = findChunk(test.scope);
-         if (chunk.has(pattern)) {
-           console.log('    Probable ' + pos + ': ' + pattern);
-           result++;
-         }
-      });
+    let result = 0;
+    let negativeTests = [];
+    let improbableTests = [];
+    let probableTests = [];
+    let positiveTests = [];
 
-      positiveTests.forEach((test) => {
-         let pattern = test.pattern.replace('%word%', word);
-         let chunk = findChunk(test.scope);
-         if (test.scope === 'sentence') {
-            console.log('#####!!!!! ' + JSON.stringify(chunk) + '!!!!!#####');
-         }
-         if (chunk.has(pattern)) {
-           console.log('    positive ' + pos + ': ' + pattern);
-           result++;
-           result++;
-        }
-      });
+    let testSet = posTests.filter((test) => test.pos === pos);
 
-      return result;
-   }
+    if (testSet.length > 0) {
+      negativeTests = testSet.filter((test) => test.type === "negative");
+      improbableTests = testSet.filter((test) => test.type === "improbable");
+      probableTests = testSet.filter((test) => test.type === "probable");
+      positiveTests = testSet.filter((test) => test.type === "positive");
+    }
 
-   function compareResults(results) {
-      const ties = [];
-      const winner = Object.keys(results).reduce((previous, current) => {
-         console.log('Ties for now include: ' + ties);
-         const diff = results[current] - results[previous];
-         console.log(current + ' - ' + previous);
-         console.log(diff);
-         switch (Math.sign(diff)) {
-            case (-1):
-               console.log('previous is bigger');
-               return previous;
-            case (0):
-               ties.push(previous);
-               console.log('it is a tie');
-               return current;
-            case (1):
-               console.log('current is bigger');
-               ties.length = 0;
-               return current;
-            default:
-               break;
-         }
-      })
+    testing(negativeTests);
+    testing(improbableTests);
+    testing(probableTests);
+    testing(positiveTests);
 
-      if (ties.length > 0) {
-         ties.push(winner);
-         return ties;
-      } else {
-         return [winner];
+    return result;
+  }
+
+  function compareResults(results) {
+    const ties = [];
+    const winner = Object.keys(results).reduce((previous, current) => {
+      console.log("Ties for now include: " + ties);
+      const diff = results[current] - results[previous];
+      console.log(current + " - " + previous);
+      console.log(diff);
+      switch (Math.sign(diff)) {
+        case -1:
+          console.log("previous is bigger");
+          return previous;
+        case 0:
+          ties.push(previous);
+          console.log("it is a tie");
+          return current;
+        case 1:
+          console.log("current is bigger");
+          ties.length = 0;
+          return current;
+        default:
+          break;
       }
-   }
+    });
 
-   ///////
-   // Main
+    if (ties.length > 0) {
+      ties.push(winner);
+      return ties;
+    } else {
+      return [winner];
+    }
+  }
 
-   const word = term.word;
-   if (!(doc.has(word))) {
-      return false;
-   }
+  ///////
+  // Main
 
-   const POSes = term.POSes.map(pos => (posNameNormalize(pos)));
+  const word = term.word;
+  if (!doc.has(word)) {
+    return false;
+  }
+
+  const POSes = term.POSes.map((pos) => posNameNormalize(pos));
 
   const results = {};
   Object.values(POSes).forEach((pos) => {
     results[pos] = 0;
   });
 
+  console.log("Term: " + word);
+  console.log("Possible POSes:" + JSON.stringify(POSes));
 
-   console.log('Term: ' + word);
-   console.log('Possible POSes:' + JSON.stringify(POSes));
+  Object.values(POSes).forEach((pos) => {
+    console.log("Testing POS: [" + pos + "]");
 
-   Object.values(POSes).forEach((pos) => {
-     console.log('Testing POS: [' + pos + ']');
+    results[pos] = isPOS(word, pos);
+  });
 
-     results[pos] = isPOS(word, pos);
-   });
+  console.log(results);
+  const winner = compareResults(results);
+  console.log(winner);
 
-   console.log(results);
-   const winner = compareResults(results);
-   console.log(winner);
-
-   if (winner.length > 1) {
+  if (winner.length > 1) {
+    return;
+  } else {
+    const disambiguatedPOS = compromiseTagged(winner[0]);
+    const docWord = doc.match(word);
+    if (docWord.has(disambiguatedPOS)) {
+      console.log("Already correct POS");
       return;
-   } else {
-      const disambiguatedPOS = compromiseTagged(winner[0]);
-      const docWord = doc.match(word);
-      if (docWord.has(disambiguatedPOS)) {
-         console.log('Already correct POS');
-         return;
-      } else {
-         console.log('Changing POS on ' + word + ' to ' + disambiguatedPOS);
-         // const oldTags = docWord.out('tags');
-         // docWord.unTag(oldTags);
-         docWord.tag(disambiguatedPOS);
-         return;
-      }
-   }
+    } else {
+      console.log("Changing POS on " + word + " to " + disambiguatedPOS);
+      // const oldTags = docWord.out('tags');
+      // docWord.unTag(oldTags);
+      docWord.tag(disambiguatedPOS);
+      return;
+    }
+  }
 }
