40f7bf8e859e6b09f4935f33a158b1cc60505168
Elbuagnin <elbuagnin@gmail.com>
2022-03-24 11:01:39 -0500

    now handles sub-sequences for cleaner sequence files

--- sequencer.js
+++ sequencer.js
@@ -1,44 +1,64 @@
-import sequence from './sequence.js';
-import parse from './parser.js';
+import * as mfs from "./lib/filesystem.js";
+import sequence from "./sequence.js";
+import parse from "./parser.js";
 
 export default function sequencer(document) {
+  function execute(instruction) {
+    const { scope } = instruction;
 
-  const sentences = document.sentences();
-  sequence.sort((a, b) => a.order - b.order);
-
-  sequence.forEach((instruction) =>  {
-    console.log(instruction);
-    const {scope} = instruction;
-
-    if (scope === 'document') {
+    if (scope === "document") {
       parse(document, instruction);
-
-   } else {
-
-       sentences.forEach((sentence) => {
-
-         if (scope === 'sentence') {
-           parse(sentence, instruction);
-
-         } else {
-
-           // Scope = 'phrase' assumed
-           let chunks = sentence;
-           if (sentence.has('#Comma')) {
-             const commas = sentence.match('#Comma');
-             commas.forEach((comma) => {
-               if (comma.ifNo('(#List|#CoordinatingAdjectives)').found) {
-                 chunks = chunks.splitAfter(comma);
-               }
-             });
-           }
+    } else {
+      sentences.forEach((sentence) => {
+        if (scope === "sentence") {
+          parse(sentence, instruction);
+        } else {
+          // Scope = 'phrase' assumed
+          let chunks = sentence;
+          if (sentence.has("#Comma")) {
+            const commas = sentence.match("#Comma");
+            commas.forEach((comma) => {
+              if (comma.ifNo("(#List|#CoordinatingAdjectives)").found) {
+                chunks = chunks.splitAfter(comma);
+              }
+            });
+          }
 
           chunks.forEach((chunk) => {
             console.log(chunk.text());
             parse(chunk, instruction);
           });
-         }
+        }
       });
-   }
- });
+    }
+  }
+
+  function subSequencer(file) {
+    const filepath = "./data/sub-sequences/" + file + ".json";
+    console.log("  Using file: " + filepath);
+    const returnType = "array";
+    const subSequence = mfs.loadJSONFile(filepath, returnType);
+    subSequence.sort((a, b) => a.order - b.order);
+
+    subSequence.forEach((subInstruction) => {
+      console.log(
+        "   Sub-Sequence Instruction: " + JSON.stringify(subInstruction)
+      );
+      execute(subInstruction);
+    });
+  }
+
+  // sequencer main
+
+  const sentences = document.sentences();
+  sequence.sort((a, b) => a.order - b.order);
+
+  sequence.forEach((instruction) => {
+    console.log(instruction);
+    if (instruction.action === "sub-sequence") {
+      subSequencer(instruction.payload.file);
+    } else {
+      execute(instruction);
+    }
+  });
 }
