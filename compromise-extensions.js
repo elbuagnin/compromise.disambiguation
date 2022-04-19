1c2870bf8b33d133b8486ed60a23c4b9197f2edd
elbuagnin <elbuagnin@gmail.com>
2022-04-18 22:31:00 -0500

    next & previous method extensions

--- compromise-extensions.js
+++ compromise-extensions.js
@@ -1,43 +1,62 @@
-import nlp from 'compromise';
+import nlp from "compromise";
 
-export function addCustomTags (tagData) { // eslint-disable-line
-    const obj = {};
-    Object.keys(tagData).forEach((k) => {
-      const tag = tagData[k];
-      obj[k] = tag;
-    });
+export function addCustomTags(tagData) {
+  const obj = {};
+  Object.keys(tagData).forEach((k) => {
+    const tag = tagData[k];
+    obj[k] = tag;
+  });
 
-    nlp.extend((_Doc, world) => {
-      world.addTags(obj);
-    })
-  }
+  nlp.extend((_Doc, world) => {
+    world.addTags(obj);
+  });
+}
 
-  export function addCustomWords (wordData) { // eslint-disable-line
-    const obj = {};
-    Object.keys(wordData).forEach((k) => {
-      const word = wordData[k];
-      obj[k] = word;
-    });
+export function addCustomWords(wordData) {
+  const obj = {};
+  Object.keys(wordData).forEach((k) => {
+    const word = wordData[k];
+    obj[k] = word;
+  });
 
-    nlp.extend((_Doc, world) => {
-      world.addWords(obj);
-    }) 
-  }
+  nlp.extend((_Doc, world) => {
+    world.addWords(obj);
+  });
+}
 
-  // _Doc.prototype.previous = function () { // eslint-disable-line
-  //   const precedingWords = this.all().before(this);
-  //   const precedingWord = precedingWords.lastTerms();
-  //
-  //   if (precedingWord.found) {
-  //     return precedingWord;
-  //   } return this.match(nothing);
-  // };
-  //
-  //   _Doc.prototype.next = function () { // eslint-disable-line
-  //   const succeedingWords = this.all().after(this);
-  //   const succeedingWord = succeedingWords.firstTerms();
-  //
-  //   if (succeedingWord.found) {
-  //     return succeedingWord;
-  //   } return this.match(nothing);
-  // };
+nlp.plugin({
+  api: (View) => {
+    (View.prototype.previous = function (quantity = 0) {
+      const precedingSegment = this.before();
+      let precedingWord = precedingSegment.lastTerms();
+      let precedingWords = precedingWord;
+
+      for (let i = 1; i < quantity; i++) {
+        precedingWords = precedingWord.union(precedingWords);
+        precedingWord = precedingWord.before().lastTerms();
+      }
+
+      if (precedingWords.found) {
+        return precedingWords;
+      } else {
+        return this.match("");
+      }
+    }),
+      (View.prototype.next = function (quantity = 0) {
+        const succedingSegment = this.after();
+        let succedingWord = succedingSegment.firstTerms();
+        let succedingWords = succedingWord;
+
+        for (let i = 1; i < quantity; i++) {
+          succedingWords = succedingWords.union(succedingWord);
+          succedingWord = succedingWord.after().firstTerms();
+        }
+
+        if (succedingWords.found) {
+          return succedingWords;
+        } else {
+          return this.match("");
+        }
+      });
+  },
+});
