a3478c8893c0b4fd5395fac3fae23ba4877aebad
Elbuagnin <elbuagnin@gmail.com>
2022-10-31 15:35:40 -0500

    duplicate data-file-structure files eliminated

--- data-interface/data-file-structure.js
+++ data-interface/data-file-structure.js
@@ -1,20 +1,45 @@
 import path from "path";
 import { fileURLToPath } from "url";
-const here = fileURLToPath(new URL('.', import.meta.url));
-const baseDir = path.join("../", here);
-const baseDataPath = path.join(baseDir, "sequencing-data");
-const classifiersPath = path.join(baseDataPath, "classifiers");
-const initializationPath = path.join(baseDataPath, "world-initialization");
+
+const here = fileURLToPath(new URL(".", import.meta.url));
+const baseDir = path.join(here, "../");
+const dataPath = path.join(baseDir, "sequencing-data");
+const classifiersPath = path.join(dataPath, "classifiers");
+const initializationPath = path.join(dataPath, "world-initialization");
 const classifierKeysPath = path.join(classifiersPath, "classification-keys");
-const classifierByTermsPath = path.join(classifiersPath, "classifications-by-terms");
-const classifierByClassificationsPath = path.join(classifiersPath, "terms-by-classifications");
-const classifierPatternsPath = path.join(classifiersPath, "classifier-patterns");
-const processorsPath = path.join(baseDataPath, "doc-processors");
-const directProcessesPath = path.join(baseDataPath, "processing");
-const subSequencesPath = path.join(baseDataPath, "sub-sequences");
-const tagPatternsPath = path.join(baseDataPath, "tag-by-patterns");
+const classifierByTermsPath = path.join(
+  classifiersPath,
+  "classifications-by-terms"
+);
+const classifierByClassificationsPath = path.join(
+  classifiersPath,
+  "terms-by-classifications"
+);
+const classifierPatternsPath = path.join(
+  classifiersPath,
+  "classifier-patterns"
+);
+const processorsPath = path.join(dataPath, "doc-processors");
+const directProcessesPath = path.join(dataPath, "processing");
+const sequencePath = path.join(dataPath, "sequence.json");
+const subSequencesPath = path.join(dataPath, "sub-sequences");
+const tagPatternsPath = path.join(dataPath, "tag-by-patterns");
 const tagsPath = path.join(initializationPath, "tags");
 const wordsPath = path.join(initializationPath, "words");
 
-export { baseDataPath, classifiersPath, initializationPath, classifierKeysPath, classifierByTermsPath, classifierByClassificationsPath, classifierPatternsPath,
-         processorsPath, directProcessesPath, subSequencesPath, tagPatternsPath, tagsPath, wordsPath }
+export {
+  dataPath,
+  classifiersPath,
+  initializationPath,
+  classifierKeysPath,
+  classifierByTermsPath,
+  classifierByClassificationsPath,
+  classifierPatternsPath,
+  processorsPath,
+  directProcessesPath,
+  sequencePath,
+  subSequencesPath,
+  tagPatternsPath,
+  tagsPath,
+  wordsPath,
+};
