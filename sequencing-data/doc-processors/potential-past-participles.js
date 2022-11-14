00943a1cc32921bcaddc94014da350f2dc3e19f3
Charles Fewlass <elbuagnin@gmail.com>
2022-11-14 03:25:42 -0600

    support for 'en' endings

--- sequencing-data/doc-processors/potential-past-participles.js
+++ sequencing-data/doc-processors/potential-past-participles.js
@@ -1,65 +1,37 @@
 import * as helpers from "./../../lib/word-helpers.js";
 
 export default function potentialPastParticiples(doc) {
+  function vowelPattern(string) {
+    function isVowel(character) {
+      const vowels = "aeiou";
+      if (vowels.includes(character)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    let pattern = "";
+    Object.values(string).forEach((character) => {
+      if (isVowel(character) === true) {
+        pattern += "v";
+      } else {
+        pattern += "c";
+      }
+    });
+
+    return pattern;
+  }
+
   function isEDpp(word) {
     function stem(text) {
-      function isVowel(character) {
-        const vowels = "aeiou";
-        if (vowels.includes(character)) {
-          return true;
-        } else {
-          return false;
-        }
-      }
-
-      function vowelPattern(string) {
-        let pattern = "";
-        Object.values(string).forEach((character) => {
-          if (isVowel(character) === true) {
-            pattern += "v";
-          } else {
-            pattern += "c";
-          }
-        });
-
-        return pattern;
-      }
-
       text = text.substring(0, text.length - 2);
       const end = text.length;
 
       // Double character end
       if (text.charAt(end - 1) === text.charAt(end - 2)) {
-        //const doubleEnd = text.substring(end - 2);
         const shorted = text.substring(0, end - 1);
         text = shorted;
-        // switch (doubleEnd) {
-        //   case "ee":
-        //     break;
-        //   case "bb":
-        //     text = shorted;
-        //     break;
-        //   case "gg":
-        //     text = shorted;
-        //     break;
-        //   case "ll":
-        //     text = shorted;
-        //     break;
-        //   case "nn":
-        //     text = shorted;
-        //     break;
-        //   case "pp":
-        //     text = shorted;
-        //     break;
-        //   case "rr":
-        //     text = shorted;
-        //     break;
-        //   case "tt":
-        //     text = shorted;
-        //     break;
-        //   default:
-        //     break;
-        // }
 
         return text;
       }
@@ -104,9 +76,44 @@ export default function potentialPastParticiples(doc) {
     }
   }
 
+  function isENpp(word) {
+    function stem(text) {
+      text = text.substring(0, text.length - 2);
+      const end = text.length;
+
+      // Double character end
+      if (text.charAt(end - 1) === text.charAt(end - 2)) {
+        const shorted = text.substring(0, end - 1);
+        text = shorted;
+      }
+
+      text = text + "e";
+      console.log(text);
+      return text;
+    }
+
+    if (word.text().substring(word.text().length - 2) === "en") {
+      let stemmed = stem(word.text());
+      if (helpers.hasPOS(stemmed, "vv")) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+
   const EDs = doc.match("/ed$/");
   EDs.forEach((word) => {
     if (isEDpp(word) === true) {
+      word.tag("#PotentialPastParticiple");
+    }
+  });
+
+  const ENs = doc.match("/en$/");
+  ENs.forEach((word) => {
+    if (isENpp(word) === true) {
       word.tag("#PotentialPastParticiple");
     }
   });
