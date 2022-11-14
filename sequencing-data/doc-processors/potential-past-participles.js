c5402d678c11fd8ed0bf3549962c98c72a126c6c
Charles Fewlass <elbuagnin@gmail.com>
2022-11-14 00:57:45 -0600

    double consonent dropped 'e' as in carved

--- sequencing-data/doc-processors/potential-past-participles.js
+++ sequencing-data/doc-processors/potential-past-participles.js
@@ -1,105 +1,108 @@
 import * as helpers from "./../../lib/word-helpers.js";
 
 export default function potentialPastParticiples(doc) {
-    function isEDpp(word) {
-        function stem(text) {
-          function isVowel(character) {
-            const vowels = "aeiou";
-            if (vowels.includes(character)) {
-              return true;
-            } else {
-              return false;
-            }
-          }
-    
-          function vowelPattern(string) {
-            let pattern = "";
-            Object.values(string).forEach(character => {
-              if (isVowel(character) === true) {
-                pattern += "v";
-              } else {
-                pattern += "c";
-              }
-            });
-    
-            return pattern;
-          }
-    
-          text = text.substring(0, text.length - 2);
-          const end = text.length;
-    
-          // Double character end
-          if (text.charAt(end - 1) === text.charAt(end - 2)) {
-            //const doubleEnd = text.substring(end - 2);
-            const shorted = text.substring(0, end - 1);
-            text = shorted;
-            // switch (doubleEnd) {
-            //   case "ee":
-            //     break;
-            //   case "bb":
-            //     text = shorted;
-            //     break;
-            //   case "gg":
-            //     text = shorted;
-            //     break;
-            //   case "ll":
-            //     text = shorted;
-            //     break;
-            //   case "nn":
-            //     text = shorted;
-            //     break;
-            //   case "pp":
-            //     text = shorted;
-            //     break;
-            //   case "rr":
-            //     text = shorted;
-            //     break;
-            //   case "tt":
-            //     text = shorted;
-            //     break;
-            //   default:
-            //     break;
-            // }
-    
-            return text;
-          }
-    
-          // Ends in 'y'
-          if (text.charAt(end - 1) === "y") {
-            if (!isVowel(text.charAt(end - 2))) {
-              text = text.substring(0, end - 1) + "ie";
-    
-              return text;
-            }
-          }
-    
-          // Dropped 'e'
-          if (vowelPattern(text.substring(end - 3, end)) === "cvc") {
-            const option1 = text;
-            const option2 = text + "e";
-    
-            if (helpers.hasPOS(option1, "vv")) {
-              return option1;
-            } else if (helpers.hasPOS(option2, "vv")) {
-              return option2;
-            }
-          }
-    
-          // Default: no adjustments
-          return text;
-        }
-    
-        if (word.text().substring(word.text().length - 2) === "ed") {
-          let stemmed = stem(word.text());
-          if (helpers.hasPOS(stemmed, "vv")) {
-            return true;
-          } else {
-            return false;
-          }
+  function isEDpp(word) {
+    function stem(text) {
+      function isVowel(character) {
+        const vowels = "aeiou";
+        if (vowels.includes(character)) {
+          return true;
         } else {
           return false;
         }
       }
+
+      function vowelPattern(string) {
+        let pattern = "";
+        Object.values(string).forEach((character) => {
+          if (isVowel(character) === true) {
+            pattern += "v";
+          } else {
+            pattern += "c";
+          }
+        });
+
+        return pattern;
+      }
+
+      text = text.substring(0, text.length - 2);
+      const end = text.length;
+
+      // Double character end
+      if (text.charAt(end - 1) === text.charAt(end - 2)) {
+        //const doubleEnd = text.substring(end - 2);
+        const shorted = text.substring(0, end - 1);
+        text = shorted;
+        // switch (doubleEnd) {
+        //   case "ee":
+        //     break;
+        //   case "bb":
+        //     text = shorted;
+        //     break;
+        //   case "gg":
+        //     text = shorted;
+        //     break;
+        //   case "ll":
+        //     text = shorted;
+        //     break;
+        //   case "nn":
+        //     text = shorted;
+        //     break;
+        //   case "pp":
+        //     text = shorted;
+        //     break;
+        //   case "rr":
+        //     text = shorted;
+        //     break;
+        //   case "tt":
+        //     text = shorted;
+        //     break;
+        //   default:
+        //     break;
+        // }
+
+        return text;
+      }
+
+      // Ends in 'y'
+      if (text.charAt(end - 1) === "y") {
+        if (!isVowel(text.charAt(end - 2))) {
+          text = text.substring(0, end - 1) + "ie";
+
+          return text;
+        }
+      }
+
+      // Dropped 'e'
+      if (
+        vowelPattern(text.substring(end - 3, end)) === "cvc" ||
+        vowelPattern(text.substring(end - 2, end)) === "cc"
+      ) {
+        const option1 = text;
+        const option2 = text + "e";
+
+        if (helpers.hasPOS(option1, "vv")) {
+          return option1;
+        } else if (helpers.hasPOS(option2, "vv")) {
+          return option2;
+        }
+      }
+
+      // Default: no adjustments
+      return text;
+    }
+
+    if (word.text().substring(word.text().length - 2) === "ed") {
+      let stemmed = stem(word.text());
+      if (helpers.hasPOS(stemmed, "vv")) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
 
   const EDs = doc.match("/ed$/");
   EDs.forEach((word) => {
